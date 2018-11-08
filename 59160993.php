ce870524ea65d17dce046a24fb9c4ed3d4e95155
ekarat2540 <ekarat.suk@hotmail.com>
2018-11-09 01:50:31 +0700

    first

---
+++ 59160993.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>59160993</title>
+    <link href="https://fonts.googleapis.com/css?family=Inconsolata|Rubik:300,400,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles-merged.css">
+    <link rel="stylesheet" href="css/style.min.css">
+    <link rel="stylesheet" href="css/custom.css">
+
+    <!--[if lt IE 9]>
+      <script src="js/vendor/html5shiv.min.js"></script>
+      <script src="js/vendor/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+  <!-- START: header -->
+
+  <header role="banner" class="probootstrap-header">
+    <div class="container-fluid">
+        <a href="59160993.html" class="probootstrap-logo">Portfolio<span>.</span></a>
+        
+        <a href="#" class="probootstrap-burger-menu visible-xs" ><i>Menu</i></a>
+        <div class="mobile-menu-overlay"></div>
+
+        <nav role="navigation" class="probootstrap-nav hidden-xs">
+          <ul class="probootstrap-main-nav">
+            <li class="active"><a href="index.html">Home</a></li>
+   
+        
+       
+          </ul>
+          <ul class="probootstrap-header-social hidden-xs">
+            <li><a href="#"><i class="icon-twitter"></i></a></li>
+            <li><a href="#"><i class="icon-facebook2"></i></a></li>
+            <li><a href="#"><i class="icon-instagram2"></i></a></li>
+          </ul>
+          <div class="extra-text visible-xs">
+            <a href="#" class="probootstrap-burger-menu"><i>Menu</i></a>
+            <h5>Connect With Me</h5>
+            <ul class="social-buttons">
+              <li><a href="#"><i class="icon-twitter"></i></a></li>
+              <li><a href="#"><i class="icon-facebook2"></i></a></li>
+              <li><a href="#"><i class="icon-instagram2"></i></a></li>
+            </ul>
+          </div>
+        </nav>
+    </div>
+  </header>
+  <!-- END: header -->
+  
+  <!-- START: section -->
+  <section class="probootstrap-intro" style="background-image: url(img/hero_bg_1_b.jpg);" data-stellar-background-ratio="0.5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-7 probootstrap-intro-text">
+          <h1 class="probootstrap-animate" data-animate-effect="fadeIn"><span>Ekarat</span> Sukprasert</h1>
+          <div class="probootstrap-subtitle probootstrap-animate" data-animate-effect="fadeIn">
+            <h2>Student of BUU</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+    <span class="probootstrap-animate"><a class="probootstrap-scroll-down js-next" href="#next-section">Scroll down <i class="icon-chevron-down"></i></a></span>
+  </section>
+  <!-- END: section -->
+  <main>
+    <section id="next-section" class="probootstrap-section">
+      <div class="container">
+        <div class="row probootstrap-gutter60 mb50">
+          <div class="col-md-6">
+            <figure><img src="img/a.jpg" lt="Free Bootstrap Template by uicookies.com" class="img-responsive"></figure>
+          </div>
+          <div class="col-md-6">
+            <h2 class="probootstrap-heading">About Me</h2>
+            <p>ชื่อเอกราช สุขประเสริฐ อายุ 21 ปี นิสิตมหาวิทยาลัยบูรพา  คณะวิทยาการสารสนเทศ สาขาวิทยาการคอมพิวเตอร์</p>
+            <p>Email : 59160993@gu.buu.ac.th</p>
+			<p>Tel : 0972311021</p>
+            <p><a href="#" class="btn btn-primary">Read more about me</a></p>
+          </div>
+        </div>
+
+        <div class="row probootstrap-gutter60">
+          <div class="col-md-4 mb30">
+            <h4 class="mb30">จบการศึกษาจาก</h4>
+            <p>จบการศึกษาจาก โรงเรียนบางกะปิ ในระดับมัธยมศึกษา</p>
+          </div>
+          <div class="col-md-4 mb30">
+            <h4 class="mb30">ปัจจุบันศึกษา</h4>
+			<p>ปัจจุบันศึกษาที่มหาวิทยาลัยบูรพา คณะวิทยาการสารสนเทศ สาขาวิทยาการคอมพิวเตอร์ ระดับชั้นปีที่ 3 แขนง Programming
+          </div>
+          <div class="col-md-4 mb30">
+            <h4 class="mb30">เป้าหมายการทำงานในอนาคต</h4>
+            <p>Programmer</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+  <footer class="probootstrap-footer">
+      <div class="probootstrap-backtotop"><a href="#" class="js-backtotop"><i class="icon-chevron-thin-up"></i></a></div>
+      <div class="container">
+      <div class="row mb50">
+        <div class="col-md-3">
+          <div class="probootstrap-footer-widget">
+            <h4>About Me</h4>
+            <p>Email : 59160993@go.buu.ac.th</p>
+			<p>Tel : 0972311021</p>
+          </div>
+        </div>
+  </footer>  
+
+  <script src="js/scripts.min.js"></script>
+  <script src="js/main.min.js"></script>
+  <script src="js/custom.js"></script>
+
+  </body>
+</html>
